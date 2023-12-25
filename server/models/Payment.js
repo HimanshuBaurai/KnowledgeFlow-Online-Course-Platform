a336a9753f9ed07dd75c64e6fa428b50d8c70ef3
Himanshu Baurai <himanshucosmicera@gmail.com>
2023-12-25 12:08:38 +0530

    payment verification and get razorpayKey routes API done

---
+++ server/models/Payment.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const paymentSchema = new mongoose.Schema(
+    {
+        razorpay_payment_id: {
+            type: String,
+            required: true
+        },
+        razorpay_subscription_id: {
+            type: String,
+            required: true
+        },
+        razorpay_signature: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }
+);
+
+export const Payment = mongoose.model("Payment", paymentSchema);
--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { isAuthenticated } from '../middlewares/auth.js';
-import { buySubscription } from '../controllers/paymentController.js';
+import { buySubscription, getRazorPayKey, paymentVerification } from '../controllers/paymentController.js';
 
 
 const router = express.Router();
 
 // Buy subscription
 router.route('/subscribe').get(isAuthenticated, buySubscription);//isAuthenticated ensures that the user is logged in
+router.route('/paymentverification').post(isAuthenticated, paymentVerification);//verify payment and save reference in db
+router.route('/razorpaykey').get(getRazorPayKey);
 
 export default router;
